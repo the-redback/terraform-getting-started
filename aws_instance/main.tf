9d50e5b700f465a3dde7cbf12f91ed9a04804e86
the-redback <maruf.2hin@gmail.com>
2023-04-13 03:17:19 -0500

    Separate varibales.tf and outputs.tf file

--- aws_instance/main.tf
+++ aws_instance/main.tf
@@ -29,14 +29,3 @@ resource "aws_security_group" "instance" {
   }
 }
 
-
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
-
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
