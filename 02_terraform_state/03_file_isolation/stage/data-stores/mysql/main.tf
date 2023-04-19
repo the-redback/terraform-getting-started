67e851f2b748d14c14782e81bc2e5d42dd03bd9e
the-redback <maruf.2hin@gmail.com>
2023-04-19 17:22:27 -0500

    FIle layout before terraform remote state

---
+++ 02_terraform_state/03_file_isolation/stage/data-stores/mysql/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "tf-state-m29"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
---
+++ 02_terraform_state/03_file_isolation/stage/data-stores/mysql/readme.md
@@ -0,0 +1,6 @@
+```bash
+export TF_VAR_db_username="(YOUR_DB_USERNAME)"
+export TF_VAR_db_password="(YOUR_DB_PASSWORD)"
+terraform plan
+terraform apply
+```
