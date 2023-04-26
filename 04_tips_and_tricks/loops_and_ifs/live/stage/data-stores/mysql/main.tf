cf0e696ffd56deae050d2f55dbd559a207318277
the-redback <maruf.2hin@gmail.com>
2023-04-26 12:08:13 -0500

    Versioned module

---
+++ 04_tips_and_tricks/loops_and_ifs/live/stage/data-stores/mysql/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "tf-state-m29"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "data_stores" {
+  source = "../../../modules/data-stores/mysql"
+
+  cluster_name = "stage_mysql"
+  db_username  = var.db_username
+  db_password  = var.db_password
+}
---
+++ 04_tips_and_tricks/loops_and_ifs/modules/data-stores/mysql/variables.tf
@@ -0,0 +1,16 @@
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
